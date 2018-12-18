fd2fd93656de59c5b7715efced262a1b8a81b84c
jyseo <sjyounghos@gmail.com>
2018-12-18 18:45:04 +0900

    펫시터 집으로 부르기 화면 작업중 주소변경 팝업창 페이지 추가

--- src/main/java/kr/co/mypet/sitter/dao/SitterDao.java
+++ src/main/java/kr/co/mypet/sitter/dao/SitterDao.java
@@ -1,12 +1,14 @@
 package kr.co.mypet.sitter.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import kr.co.mypet.insurance.model.MypetVo;
 import kr.co.mypet.sitter.model.FaqVo;
 import kr.co.mypet.sitter.model.PetSitterVo;
 import kr.co.mypet.sitter.model.ZipVo;
@@ -45,5 +47,15 @@ public class SitterDao implements SitterDaoInf {
 	@Override
 	public FaqVo getFaqOne(String psf_id) {
 		return template.selectOne("petSitter.getFaqOne", psf_id);
+	}
+
+	@Override
+	public int memAddrUpdate(Map<String, Object> param) {
+		return template.update("petSitter.memAddrUpdate", param);
+	}
+
+	@Override
+	public List<MypetVo> getMypetList(String mem_id) {
+		return template.selectList("petSitter.getMypetList", mem_id);
 	}	
 }
